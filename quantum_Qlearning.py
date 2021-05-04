3e3dfc194460b15dd65dcc8ad83874e451d5bfb4
Pol <bracciapaolo@gmail.com>
2021-05-04 18:11:14 +0200

    sgruba

--- quantum_Qlearning.py
+++ quantum_Qlearning.py
@@ -1,1 +1,10 @@
-__author__ = 'pol'
+__author__ = 'sgruba'
+
+"""Class reproducing 2103.15084"""
+
+class QuantumQlearner:
+    """
+    Inits a quantum QLearner object.
+    """
+    def __init__(self, args):
+        robe
